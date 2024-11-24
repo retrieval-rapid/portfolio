2d2e44f6fe1803747871900a0771a22104ccc8e6
superroots <bajrabeings@gmail.com>
2024-11-24 18:32:42 +0545

    code refactors

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,27 +1,25 @@
-import { Text } from './text';
+import { Text } from './ui/text';
 import Link from 'next/link';
-import { Button } from './button';
+import { Button } from './ui/button';
 import { SOCIAL_LINKS } from '@/data/links';
 
 export function Hero() {
   return (
-    <div className="container px-4 py-2 flex flex-col gap-8">
-      {/* FIRST */}
+    <div className="container flex flex-col gap-8 px-4 py-2 mb-6 border-b border-b-neutral-300">
       <Text variant="h2">
-        Full Stack
-        <br /> Developer
+        Full Stack <br /> Developer
       </Text>
-
-      {/* SECOND */}
       <div className="grid grid-cols-5 gap-8">
         <div className="col-span-5 lg:col-span-3 xl:col-span-4">
-          <Text variant="body-sr" className="font-light tracking-tighter leading-loose">
+          <Text variant="body-sr" className="font-light tracking-wider leading-loose">
             Specialized in Web Design, UX / UI, Framer, and Front End Development. <br />
             I'm helping startups and business owners to make a visual statement through spotless Web
             Design and Development so they can increase brand awareness and sell more.
           </Text>
           <Button variant="default" className="relative !bg-black text-white rounded-3xl pr-8 mt-5">
-            <Text variant="body-esr">Drop me line</Text>
+            <Text variant="body-esr" className="tracking-wider">
+              Drop me line
+            </Text>
             <span className="animate-pulse bg-white rounded-full size-2 absolute right-2 top-1/2 -translate-y-1/2"></span>
           </Button>
         </div>
@@ -29,13 +27,13 @@ export function Hero() {
           <div className="flex flex-col gap-4">
             <Text
               variant="body-sr"
-              className="text-gray-500 tracking-wide underline underline-offset-8"
+              className="text-gray-500 tracking-wider underline underline-offset-8"
             >
               Let's get connected
             </Text>
             {SOCIAL_LINKS.map((link, index) => (
               <Link href={link.href} key={index}>
-                <span className="text-sm tracking-wide">{link.text}</span>
+                <span className="text-sm tracking-wider">{link.text}</span>
               </Link>
             ))}
           </div>
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,13 +1,13 @@
-import { Text } from './text';
+import { Text } from './ui/text';
 import { SKILLS } from '@/data/skills';
 
 export function Skills() {
   return (
-    <div className="flex flex-col gap-8 px-4">
-      <Text variant="body-sr" className="text-2xl tracking-wider">
+    <div className="flex flex-col gap-8 px-4 py-2 mb-6 border-b border-b-neutral-300">
+      <Text variant="body-sr" className="text-2xl tracking-wider font-semibold">
         I can help you with ...
       </Text>
-      <div className="flex justify-between flex-wrap gap-4">
+      <div className="flex justify-between flex-wrap gap-4 mb-4">
         {SKILLS.map((skill, index) => (
           <div key={index} className="w-72 border-l-2 px-4 py-2">
             <div className='flex flex-col gap-4'>
@@ -15,10 +15,10 @@ export function Skills() {
                 {String(index + 1).padStart(2, '0')}
               </Text>
               <div className="flex flex-col gap-2">
-                <Text variant="body-sr" className="text-xl tracking-wide">
+                <Text variant="body-sr" className="text-xl tracking-wider">
                   {skill.title}
                 </Text>
-                <Text variant="body-esr" className="text-justify leading-4 tracking-tight">
+                <Text variant="body-esr" className="text-justify leading-4 tracking-wider">
                   {skill.description}
                 </Text>
               </div>
