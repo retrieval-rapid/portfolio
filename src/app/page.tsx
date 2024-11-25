237d292f340ede7f26757a53814a792066098178
superroots <bajrabeings@gmail.com>
2024-11-26 00:45:50 +0545

    fixes overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Skills } from '@/components/skills';
 
 export default function Home() {
   return (
-    <main className="overflow-x-hidden max-w-[1440px] mx-auto selection:bg-teal-200">
+    <main className="w-full mx-auto selection:bg-teal-200">
       <div className='fixed inset-0 h-screen w-screen -z-10'>
         <div className="absolute inset-0 -z-10 size-full max-h-screen max-w-screen bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
           <div className="absolute inset-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]" />
